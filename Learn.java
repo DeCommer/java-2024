1ff918ee686c95b10c75da87069bdddff502be50
Joseph DeCommer <jdecommer@gmail.com>
2024-07-11 08:23:36 -0400

    Java daily practice

---
+++ Learn.java
@@ -0,0 +1,14 @@
+public class Learn {
+    
+    public static void main(String[] args) {
+
+        int x = 12;
+        int y = 2;
+
+        int z = x + y;
+
+        System.out.println(z);      
+
+
+    }
+}
