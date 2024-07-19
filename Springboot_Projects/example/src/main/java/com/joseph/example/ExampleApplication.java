4f8e77ea69d79a2b620f10003a93dbd3c200aaa5
Joseph DeCommer <jdecommer@gmail.com>
2024-07-19 08:33:40 -0400

    Added formatted string output example

--- Springboot_Projects/example/src/main/java/com/joseph/example/ExampleApplication.java
+++ Springboot_Projects/example/src/main/java/com/joseph/example/ExampleApplication.java
@@ -8,7 +8,13 @@ public class ExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExampleApplication.class, args);
-		
+        String name = "Joseph DeCommer";
+		String country = "U.S.A.";
+		int age = 44;
+		double height = Math.floor((5.9 * 12) * 100) / 100;
+
+		String formatted = String.format("my name is %s. I am from the %s. I am %d years old and I am %f in tall", name, country, age, height);
+		System.out.println(formatted);
 	}
 
 }
