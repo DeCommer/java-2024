d10ad0e8343b14bfa090ee328a40d96d3e0f1886
Joseph DeCommer <jdecommer@gmail.com>
2024-07-21 16:54:44 -0400

    added Math practice file

--- Springboot_Projects/example/src/main/java/com/joseph/example/ExampleApplication.java
+++ Springboot_Projects/example/src/main/java/com/joseph/example/ExampleApplication.java
@@ -1,5 +1,7 @@
 package com.joseph.example;
 
+import java.util.Random;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,13 +10,15 @@ public class ExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExampleApplication.class, args);
-        String name = "Joseph DeCommer";
-		String country = "U.S.A.";
-		int age = 44;
-		double height = Math.floor((5.9 * 12) * 100) / 100;
 
-		String formatted = String.format("my name is %s. I am from the %s. I am %d years old and I am %f in tall", name, country, age, height);
-		System.out.println(formatted);
+ 			Random rand = new Random();
+            int n = rand.nextInt(100);
+			double test = Math.pow(n, 2);
+            System.out.printf("Random number: %d", n);
+			System.out.println("");
+            System.out.printf("Random number squared: %f", test);
+			System.out.println("");
+
 	}
 
 }
