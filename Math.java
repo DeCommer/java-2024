a9b0f747fc8e830c50cd28ec0e8f87a57f9e9756
Joseph DeCommer <jdecommer@gmail.com>
2024-07-22 09:39:58 -0400

    added avg function

--- Math.java
+++ Math.java
@@ -1,4 +1,7 @@
+
 import java.util.Random;
+
+
 
 public class Math
     {
@@ -6,20 +9,29 @@ public class Math
         {
             //Calculate an average from an array of numbers
             int[] nums = {25, 50, 40, 70, 80, 30};
-            int average = 0;
-            int count = 0;
-            for(int i = 0; i < nums.length; i++)
-                {
-                    count++;
-                    average += nums[i];
-                    System.out.print(nums[i] + " ");
-                }
-            System.out.println("");
-            try {
-                double averageOut = (double) average / count;
-                System.out.println("The average is: " + averageOut);
-            } catch(ArithmeticException e) {
-                System.out.println("Divide by zero error");
-            }  
+            avg(nums);
         }
+
+        public static int[] avg(int[] arr) 
+            {
+                int average = 0;
+                int count = 0;
+                for(int i = 0; i < arr.length; i++)
+                    {
+                        count++;
+                        average += arr[i];
+                        System.out.print(arr[i] + " ");
+                    }
+                System.out.println("");
+                try
+                    {
+                        double averageOut = (double) average / count;
+                        System.out.println("The average is: " + averageOut);
+                    }
+                catch(ArithmeticException e)
+                    {
+                        System.out.println("Divide by zero error");
+                    } 
+                    return arr;
+            }
     }
