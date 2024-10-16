073cce996fdd034aae2202f3a4687395f2b615df
Joseph DeCommer <jdecommer@gmail.com>
2024-10-16 13:15:25 -0400

    deleted files

--- Lambda.java
+++ Lambda.java
@@ -1,27 +1,22 @@
 //Lambda only works with functional interface (a method that returns something)
 @FunctionalInterface
-interface A
-    {
+interface A{
         int add(int i, int j);
-        // int sub(int i, int j);
+}
+
+interface S{
+        int sub(int i, int j);
+}
+
+public class Lambda{
+    public static void main(String[] args){
+
+            A addition = (int i, int j) -> i + j; // turns into this.
+            S subtraction = (int i, int j) -> i - j;
+
+            int result = addition.add(5, 4);
+            int subRes = subtraction.sub(5, 4);
+            System.out.println(result);
+            System.out.println(subRes);
     }
-
-public class Lambda
-    {
-        public static void main(String[] args)
-            {
-
-                // A obj = new A()
-                //     {
-                //         public int add(int i, int j)
-                //             {  //this ...
-                //                 return i + j;
-                //             }
-                //     };
-
-                A addition = (int i, int j) -> i + j; // turns into this.
-
-                int result = addition.add(5, 4);
-                System.out.println(result);
-            }
-    }
+}
