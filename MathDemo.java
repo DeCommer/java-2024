4767ad0a21fe913d15ef791871aa8c38661f6c1a
Joseph DeCommer <jdecommer@gmail.com>
2024-10-21 16:22:24 -0400

    Java fun

--- MathDemo.java
+++ MathDemo.java
@@ -1,3 +1,6 @@
+///Here's a fun one.
+/// Create a user entered array length with random elements
+/// return the array element total, array sum, and average
 
 
 public class MathDemo
@@ -5,25 +8,28 @@ public class MathDemo
         public static void main(String[] args)
             {
                 //Calculate an average from an array of numbers
-                int[] nums = {25, 50, 40, 70, 80, 30};
+                int[] nums = {20, 50, 40, 70, 80, 60, 30};
                 avg(nums);
             }
 
         public static int[] avg(int[] arr) 
             {
-                int average = 0;
+                int arrTotal = 0;
                 int count = 0;
                 for(int i = 0; i < arr.length; i++)
                     {
                         count++;
-                        average += arr[i];
+                        arrTotal += arr[i];
                         System.out.print(arr[i] + " ");
                     }
                 System.out.println("");
                 try
                     {
-                        double averageOut = (double) average / count;
-                        System.out.println("The average is: " + averageOut);
+                        System.out.printf("Array total: %d \n", arrTotal);
+                        System.out.printf("Elements in array: %d\n", count);
+
+                        double averageOut = (double) arrTotal / count;
+                        System.out.printf("The average is: %d / %d = %.2f",arrTotal, count, averageOut);
                     }
                 catch(ArithmeticException e)
                     {
