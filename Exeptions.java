12712104d247c810eaf50afeac26040f4c625000
Joseph DeCommer <jdecommer@gmail.com>
2024-10-23 07:44:45 -0400

    java fun

--- Exeptions.java
+++ Exeptions.java
@@ -11,48 +11,42 @@ class DeCommerExeption extends Exception
             }
     }
 
-public class Exeptions 
-    {
+public class Exeptions{
 
-        public static void main(String[] args) throws IOException
-        {
-            int i = 2;
-            int j = 0;
+    public static void main(String[] args) throws IOException{
+        int i = 2;
+        int j = 0;
 
-            try {
-                j = 18/j;
-                if(j == 0)
-                    {
-                        throw new DeCommerExeption("Zero in denominator " );
-                    }
-                }catch(ArithmeticException e) 
-                {
-                    j = 18/i;
-                    System.out.println("Zero in denominator results in default " + e);
-                }catch(Exception e)
-                {
-                    System.out.println("Not running " + e);
-                }
-            System.out.println(j);
-            System.out.println("End");
-
-            /************************/
-
-            Scanner input = new Scanner(System.in);
-            
-            InputStreamReader in = new InputStreamReader(System.in);
-            BufferedReader bf = new BufferedReader(in);
-
-            System.out.print("Enter a number: ");
-            int num2 = Integer.parseInt(bf.readLine());
-            System.out.print("Enter a number: ");
-            int num3 = input.nextInt();
-
-            System.out.println("You entered " + num2 + " From BufferReader");
-            System.out.println("You entered " + num3 + " From Scanner");
-
-            input.close();
-            bf.close();
-
+        try {
+            j = 18/j;
+            if(j == 0){
+                throw new DeCommerExeption("Zero in denominator " );
             }
+        }catch(ArithmeticException e) {
+                j = 18/i;
+                System.out.println("Zero in denominator results in default " + e);
+            }catch(Exception e){
+                System.out.println("Not running " + e);
+            }
+        System.out.println(j);
+        System.out.println("End");
+
+        /************************/
+
+        Scanner input = new Scanner(System.in);
+        
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader bf = new BufferedReader(in);
+
+        System.out.print("Enter a number: ");
+        int num2 = Integer.parseInt(bf.readLine());
+        System.out.print("Enter a number: ");
+        int num3 = input.nextInt();
+
+        System.out.println("You entered " + num2 + " From BufferReader");
+        System.out.println("You entered " + num3 + " From Scanner");
+
+        input.close();
+        bf.close();
+        }
     }
