e78ddb43c8e27af070fa2b12d960d3cbfe9fa8a1
Joseph DeCommer <jdecommer@gmail.com>
2024-11-16 14:27:18 -0500

    triangle stuff

--- Practice.java
+++ Practice.java
@@ -1,11 +1,13 @@
 public class Practice {
     
-
     public static void main(String[] args) {
-        
-        
 
-        
+        double a = 3.0;
+        double b = 4.0;
 
+        double c = Math.hypot(a, b);
+
+
+        System.out.println(c);
     }
 }
