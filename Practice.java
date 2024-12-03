0f1e69cf6a46b054ebe09ca5e6cb8398ccb9b8de
Joseph DeCommer <jdecommer@gmail.com>
2024-12-03 06:43:07 -0500

    misc

--- Practice.java
+++ Practice.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class Practice {
     
     public static void main(String[] args) {
 
+        Scanner in = new Scanner(System.in);
+        
+        
+        
         double a = 3.0;
         double b = 4.0;
 
