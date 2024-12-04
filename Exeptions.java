a22fe26830f077b3d1bb7e39abf95c49975767b7
Joseph DeCommer <jdecommer@gmail.com>
2024-12-04 11:43:45 -0500

    nc

--- Exeptions.java
+++ Exeptions.java
@@ -1,6 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.BufferedReader;
 import java.util.Scanner;
 
 class DeCommerExeption extends Exception
@@ -25,28 +25,22 @@ public class Exeptions{
         }catch(ArithmeticException e) {
                 j = 18/i;
                 System.out.println("Zero in denominator results in default " + e);
-            }catch(Exception e){
+            }catch(DeCommerExeption e){
                 System.out.println("Not running " + e);
             }
         System.out.println(j);
         System.out.println("End");
-
-        /************************/
-
-        Scanner input = new Scanner(System.in);
-        
-        InputStreamReader in = new InputStreamReader(System.in);
-        BufferedReader bf = new BufferedReader(in);
-
-        System.out.print("Enter a number: ");
-        int num2 = Integer.parseInt(bf.readLine());
-        System.out.print("Enter a number: ");
-        int num3 = input.nextInt();
-
-        System.out.println("You entered " + num2 + " From BufferReader");
-        System.out.println("You entered " + num3 + " From Scanner");
-
-        input.close();
+        BufferedReader bf;
+        try ( /************************/ Scanner input = new Scanner(System.in)) {
+            InputStreamReader in = new InputStreamReader(System.in);
+            bf = new BufferedReader(in);
+            System.out.print("Enter a number: ");
+            int num2 = Integer.parseInt(bf.readLine());
+            System.out.print("Enter a number: ");
+            int num3 = input.nextInt();
+            System.out.println("You entered " + num2 + " From BufferReader");
+            System.out.println("You entered " + num3 + " From Scanner");
+        }
         bf.close();
         }
     }
