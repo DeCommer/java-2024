3d7cf45b852ba0410a4941ef8f4d45a6c6b39155
Joseph DeCommer <jdecommer@gmail.com>
2024-12-05 08:49:07 -0500

    Changed to accept user input

--- Practice.java
+++ Practice.java
@@ -4,17 +4,17 @@ public class Practice {
     
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
-        
-        
-        
-        double a = 3.0;
-        double b = 4.0;
-
-        double c = Math.hypot(a, b);
-
-
-        System.out.println(c);
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.print("Enter first number: ");
+            double a = in.nextDouble();
+            
+            System.out.print("Enter Second number: ");
+            double b = in.nextDouble();
+            
+            double c = Math.hypot(a, b);
+            
+            System.out.println(c);
+        }
     }
 }
  
