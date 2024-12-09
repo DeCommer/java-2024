df82b4b4b70874b3d908a9d334e4bcb5d8ea1580
Joseph DeCommer <jdecommer@gmail.com>
2024-12-09 07:16:21 -0500

    practice

--- fizzyb.java
+++ fizzyb.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
 
 class FizzBuzz {
-
     public static void main(String[] args) {
-        
-    }
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter number would you like to fizzzbuzz up to: ");
+
+        int userIn = input.nextInt();
+
+        for(int i = 0; i < userIn; i++) {
+            if(i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Fizzbuzz");
+            }else if(i % 3 == 0) {
+                System.out.println("Fizz");
+            }else if( i % 5 == 0) {
+                System.out.println("Buzz");
+            }else {
+            System.out.println(i);
+            }
+        }
+        input.close();
+    }
 }
